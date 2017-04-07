9491e185c9278dbdd060254513662f24baa5b4d1
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-07 15:28:23 -0700

    add basic style elements with CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 //*--Business Logic--*//
 
-// Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
-
 function Pizza (size) {
   this.size = size;
   this.topping = [];
@@ -11,8 +9,6 @@ function Pizza (size) {
 Pizza.prototype.finalCost = function() {
   return total = parseFloat(this.cost) + parseFloat(this.size) + parseFloat(this.topping.length);
 };
-
-
 
 //*--User Interface--*//
 $(document).ready(function() {
@@ -28,66 +24,25 @@ $(document).ready(function() {
 
 //name//
     var customerName = $("input#name").val();
-    $(".name-receipt").text(customerName);
+    $(".name-receipt").text(customerName.charAt(0).toUpperCase() + customerName.slice(1));
 
 //size//
     newOrder.size = $("select#size").val();
     // newOrder.size.push(pizzaSize);
-console.log(newOrder.size);
 
 //topping//
     $("input:checkbox[name=topping]:checked").each(function(){
       var allTopping = $(this).val();
-console.log(allTopping);
 
 //push both to Pizza cosnstructor//
       newOrder.topping.push(allTopping);
     });
 
-//get final cost of order//
+//get final cost of order & show receipt//
     $("span.order-total-receipt").text(newOrder.finalCost());
+    $(".receipt").show();
+    $("#input, #order-submit").hide();
 
-console.log(newOrder.topping);
-console.log(newOrder.size);
 console.log(newOrder.finalCost());
   });
 });
-// unction Pizza (size){
-//   this.size = size;
-//   this.toppings = [];
-// }
-//
-// Pizza.prototype.sizePrice=function(){
-//   return 10 + this.size + this.toppings.length;
-// }
-//
-// $(function(){
-//   $("form#pizza-choices").submit(function(event){
-//     event.preventDefault();
-//
-//     var pizzaSize = parseInt($("input:radio[name=size]:checked").val());
-//     var newPizza = new Pizza(pizzaSize);
-//
-//     $("input:checkbox[name=topping]:checked").each(function(){
-//       var inputToppings = $(this).val();
-//       newPizza.toppings.push(inputToppings);
-//     });
-//
-//     newPizza.toppings.forEach(function(topping){
-//       $("ul.output-list").append("<li>" + topping + "</li>");
-//     });
-//
-//     $("#price").text(newPizza.sizePrice());
-//     $("#result").show();
-//     $("button#order").hide();
-//     $("button#order-again").show();
-//   });
-//
-//   $("button#order-again").click(function() {
-//     $("form")[0].reset();
-//     $("ul.output-list").empty();
-//     $("button#order").show();
-//     $("#result").hide();
-//     $("button#order-again").hide();
-//   });
-// });
