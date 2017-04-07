4706311948c17e39d4baf6b0d8e6906e1ba1ed7f
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-07 10:52:33 -0700

    change drop down menu for toppings to a checkbox, get topping value, and push topping values to pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-/*--Business Logic--*/
+//*--Business Logic--*//
 
-Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+// Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 
-function Pizza (size, topping, subtotal) {
+function Pizza (size) {
   this.size = size;
-  this.topping = topping;
+  this.topping = [];
   this.cost = 0;
-  this.subtotal = subtotal;
 }
 
 Pizza.prototype.finalCost = function (finalCost) {
@@ -15,6 +14,36 @@ Pizza.prototype.finalCost = function (finalCost) {
 
 
 
-/*--User Interface--*/
+//*--User Interface--*//
 $(document).ready(function() {
-});
+  //new order//
+  var topping = 0
+  var size = 0
+  var newOrder = new Pizza(size);
+
+  //customer name//
+  $("form.customer-name").submit(function(event) {
+    event.preventDefault();
+    var customerName = $("input#name").val();
+    $(".name-receipt").text(customerName);
+  });
+
+  //submit function//
+  $("form.pizza-order").submit(function(event) {
+    event.preventDefault();
+    var newOrder = new Pizza(size);
+
+    //size//
+    var size = parseInt($("select#size").val());
+    console.log(size);
+
+    //topping//
+    $("input:checkbox[name=topping]:checked").each(function(){
+       var allTopping = $(this).val();
+    //push both to Pizza cosnstructor//
+    newOrder.topping.push(allTopping)
+    });
+console.log(newOrder.topping);
+  });
+
+  });
